91cdf432550ad93675f0c7099ee81eba9a645c84
Stella Kafetzoglou <kafetzo@gmail.com>
2020-07-08 22:45:13 +0300

    Removed application-context xml. Added Component scan to configuration

--- src/main/java/gr/cosmote/springExercise/SpringExerciseApplication.java
+++ src/main/java/gr/cosmote/springExercise/SpringExerciseApplication.java
@@ -10,9 +10,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringExerciseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringExerciseApplication.class, args);
 
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
+		ApplicationContext ctx = SpringApplication.run(SpringExerciseApplication.class, args);
 
 		User user = (User)ctx.getBean("user");
 		user.getVehicle().drive();
--- src/main/java/gr/cosmote/springExercise/config/CoreConfiguration.java
+++ src/main/java/gr/cosmote/springExercise/config/CoreConfiguration.java
@@ -3,10 +3,12 @@ package gr.cosmote.springExercise.config;
 import gr.cosmote.springExercise.entities.Car;
 import gr.cosmote.springExercise.entities.MotorBike;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
+@ComponentScan("gr.cosmote.springExercise.entities")
 public class CoreConfiguration {
 
 
