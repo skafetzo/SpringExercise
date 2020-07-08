24a6cb60c458020e2767b821ded00358f48dec16
Stella Kafetzoglou <kafetzo@gmail.com>
2020-07-08 23:30:18 +0300

    Fixed Core configuration

--- src/main/java/gr/cosmote/springExercise/config/CoreConfiguration.java
+++ src/main/java/gr/cosmote/springExercise/config/CoreConfiguration.java
@@ -1,8 +1,6 @@
 package gr.cosmote.springExercise.config;
 
-import gr.cosmote.springExercise.entities.Car;
-import gr.cosmote.springExercise.entities.MotorBike;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,14 +8,5 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("gr.cosmote.springExercise.entities")
 public class CoreConfiguration {
 
-
-    @Bean(name = "motorbike")
-    public MotorBike getBike() {
-        return new MotorBike();
-    }
-
-
-    @Bean(name = "mycar")
-    public Car getCar() { return new Car();}
 
 }
